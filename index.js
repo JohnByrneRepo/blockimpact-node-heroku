c145784c37b6b5b5bd18ec7c2c76c376d9872af6
John Byrne <html5css3@outlook.com>
2018-08-08 14:22:21 +0100

    Update server

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const pg = require('pg');
 var connectionString = `postgres://faidqtllatsqwe:1a3f272127e1f071490f6f3c284f0653f55406113316aafa8d86dc1c286d8930@ec2-54-225-76-201.compute-1.amazonaws.com:5432/d9r70ap1bmoqk0`;
 
+const config = {
+    user: 'faidqtllatsqwe',
+    database: 'd9r70ap1bmoqk0',
+    password: '1a3f272127e1f071490f6f3c284f0653f55406113316aafa8d86dc1c286d8930',
+    port: 5432
+};
+
 const SocketServer = require('ws').Server;
 var express = require('express');
 var path = require('path');
@@ -24,6 +31,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+const pool = new pg.Pool(config);
 
 app.post('/create-user', function(req, res) {
   console.log('req.body');
@@ -33,21 +41,20 @@ app.post('/create-user', function(req, res) {
     id = req.body.id,
     email = req.body.email;
 
-    pg.connect(connectionString, function(err,client,done) {
-      if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-      } 
+    pool.connect(function (err, client, done) {
+      if (err) {
+        console.log("Can not connect to the DB" + err);
+      }
       client.query('INSERT INTO users (firstname, lastname, id, email) values($1, $2, $3, $4)',
         [firstname, lastname, id, email], function(err, result) {
-           done(); // closing the connection;
-           if(err){
-               console.log(err);
-               res.status(400).send(err);
-           }
-           res.status(200).send({ message: 'success' });
-      });
-    });
+          done();
+          if (err) {
+            console.log(err);
+            res.status(400).send(err);
+          }
+          res.status(200).send({ message: 'success' });
+        })
+      })
 });
 
 
