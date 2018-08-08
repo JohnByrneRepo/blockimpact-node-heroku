794bc05eaeb69873e5a737a6707a22ac28f9e766
John Byrne <html5css3@outlook.com>
2018-08-08 12:00:32 +0100

    Update server

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const INDEX = path.join(__dirname, 'index.html');
 // client.connect();
 
 const app = express()
+  .use(bodyParser.json())       // to support JSON-encoded bodies
+  .use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+  }))
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.use(bodyParser.json());         // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
 
 
 const ws = new SocketServer({ app });
