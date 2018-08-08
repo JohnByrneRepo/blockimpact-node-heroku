208e68d565b3af6b4c132a76ff19c0978d658b99
John Byrne <html5css3@outlook.com>
2018-08-08 12:27:59 +0100

    Update server

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-'use strict';
-
 const session = require('express-session');
 const express = require('express');
 const http = require('http');
 const uuid = require('uuid');
-const WebSocket = require('ws');
-const path = require('path');
-const pg = require('pg');
-const fs = require('fs');
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
 
-var connectionString = `postgres://faidqtllatsqwe:1a3f272127e1f071490f6f3c284f0653f55406113316aafa8d86dc1c286d8930@ec2-54-225-76-201.compute-1.amazonaws.com:5432/d9r70ap1bmoqk0`;
+const WebSocket = require('ws');
 
 const app = express();
 
@@ -28,40 +20,34 @@ const sessionParser = session({
 //
 // Serve static files from the 'public' folder.
 //
-
-app.post('/create-user', function(req, res) {
-  var firstname = req.body.firstname,
-    lastname = req.body.lastname,
-    id = req.body.id,
-    email = req.body.email;
-
-    pg.connect(connectionString,function(err,client,done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       } 
-       client.query('INSERT INTO users (firstname, lastname, id, email) values($1, $2, $3, $4)',
-        [firstname, lastname, id, email], function(err, result) {
-           done(); // closing the connection;
-           if(err){
-               console.log(err);
-               res.status(400).send(err);
-           }
-           res.status(200).send({ message: 'success' });
-       });
-    });
-});
+app.use(express.static('/'));
+app.use(sessionParser);
 
 app.get('/', function(request, response){
-    response.sendfile(INDEX);
+    response.sendfile('index.html');
+});
+
+app.post('/login', (req, res) => {
+  //
+  // "Log in" user and set userId to session.
+  //
+  const id = uuid.v4();
+
+  console.log(`Updating session for user ${id}`);
+  req.session.userId = id;
+  res.send({ result: 'OK', message: 'Session updated' });
+});
+
+app.delete('/logout', (request, response) => {
+  console.log('Destroying session');
+  request.session.destroy();
+  response.send({ result: 'OK', message: 'Session destroyed' });
 });
 
 //
 // Create HTTP server by ourselves.
 //
-const server = http.createServer(app)
-
-
+const server = http.createServer(app);
 
 const wss = new WebSocket.Server({
   verifyClient: (info, done) => {
@@ -79,32 +65,16 @@ const wss = new WebSocket.Server({
   server
 });
 
-function broadcast(data) {
-  var data = JSON.parse(data).data;
-  ws.clients.forEach((client) => {
-    client.send(JSON.stringify({
-      data: {
-        type: 'chatMessage',
-        value: data.value,
-        source: data.source
-      }
-    }));
-  });
-}
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-
-  ws.on('message', function incoming(data) {
-    console.log(data);
-    broadcast(data);
+wss.on('connection', (ws, req) => {
+  ws.on('message', (message) => {
+    //
+    // Here we can now use session parameters.
+    //
+    console.log(`WS message ${message} from user ${req.session.userId}`);
   });
 });
 
 //
 // Start the server.
 //
-
-server
-  .listen(PORT, () => console.log('Listening on http://localhost:8080'));
+server.listen(8080, () => console.log('Listening on http://localhost:8080'));
